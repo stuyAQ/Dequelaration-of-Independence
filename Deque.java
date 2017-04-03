56b26d5b8adbdec10f7b561007b46f1adc819941
Andrew Qu <aqu3@stuy.edu>
2017-04-02 20:51:14 -0400

    commented out most methods

--- Deque.java
+++ Deque.java
@@ -1,11 +1,11 @@
 public interface Deque<T> {
 
     void addLast(T e);
-    void addFirst(T e);
-    T pollLast();
-    T pollFirst();
-    T peekLast();
-    T peekFirst();
-    int size();
+    //void addFirst(T e);
+    //T pollLast();
+    //T pollFirst();
+    //T peekLast();
+    //T peekFirst();
+    //int size();
 
 }
